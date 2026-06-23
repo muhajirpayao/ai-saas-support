018bfa282a43290b08dbb7005623537ee9d9b084
Muhajir Payao <YOUR_GITHUB_EMAIL>
2026-06-23 09:43:06 +0800

    adding auth UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import SupportAi from './Supportaidashboard.jsx'
+import Landpage from './pages/Landpage.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Landpage />
   </StrictMode>,
 )
